50303f3b71eb65348b7e5c231717fb4531c9bdd3
mirmakhmudov16 <mr.mirmakhmudov16112008@gmail.com>
2024-04-01 12:37:31 +0500

    Commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import asyncio
+import logging
+from aiogram import Dispatcher, F, types
+from aiogram.enums import ParseMode
+from aiogram.filters import Command
+from config import TOKEN
+from aiogram import Bot
+
+dp = Dispatcher()
+
+
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer(f"Assalomu Aleykum, Xurmatli {message.from_user.mention_html()}")
+
+
+async def main() -> None:
+    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+    await dp.start_polling(bot, polling_timeout=1)
+
+
+if __name__ == "main":
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
